b8cc8a5c6d4ed569dbe8f73f9650e1acce2baf56
b083040012 <b083040012@g-mail.nsysu.edu.tw>
2021-11-01 03:36:26 +0800

    add rotate,zoom/shrink and histogram function

--- HW1/DIP_GUI.py
+++ HW1/DIP_GUI.py
@@ -7,6 +7,31 @@ from PIL import  Image,ImageTk
 import cv2
 
 
+def bilinear(array,x,y):
+
+    p1=min(int(x),array.shape[0]-1)
+    q1=min(int(y),array.shape[1]-1)
+    mu=x-p1
+    lamb=y=q1
+    p2=min(p1+1,array.shape[0]-1)
+    q2=min(q1+1,array.shape[1]-1)
+
+    leftUp=array[p1,q1]
+    leftDown=array[p2,q1]
+    rightUp=array[p1,q2]
+    rightDown=array[p2,q2]
+
+    value1=mu*rightDown+(1-mu)*leftDown
+    value2=mu*rightUp+(1-mu)*leftDown
+
+    level=lamb*value1+(1-lamb)*value2
+
+
+    return level
+
+
+
+
 class DIPGUI(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -25,27 +50,39 @@ class DIPGUI(Frame):
         self.selectBtn = Button(master, text='Open', command=self.selectFile)
         self.saveBtn=Button(master,text='Save',command=self.saveFile)
         self.methodBtn=Button(master,text='Linearly',command=self.methodMode)
+        self.histoBtn=Button(master,text='Histogram',command=self.histogram)
 
         # set scale
         self.methodAScale=Scale(master,orient=HORIZONTAL,from_=0,to=5,resolution=0.5,length=300,command=self.method)
         self.methodAScale.set(1)
         self.methodBScale=Scale(master,orient=HORIZONTAL,from_=-100,to=100,resolution=5,length=300,command=self.method)
         self.methodBScale.set(0)
+        self.sizeChangeScale=Scale(master,orient=HORIZONTAL,from_=-5,to=1,resolution=1,length=300,command=self.sizeChange)
+        self.sizeChangeScale.set(0)
+        self.rotateScale=Scale(master,orient=HORIZONTAL,from_=-180,to=180,resolution=1,length=300,command=self.rotate)
+        self.rotateScale.set(0)
 
         # set label showing image
         self.oriLbl=Label(bg="white",text="original",font=("Courier",30))
         self.modLbl=Label(bg='white',text='modified',font=("Courier", 30))
+        self.sizeChangeLbl=Label(bg='blue',text='Zoom/Shrink',font=("Courier",10))
+        self.rotateLbl=Label(bg='blue',text='Rotate',font=("Courier",10))
 
         # place all object on frame
         self.selectBtn.place(x=100,y=50)
         self.saveBtn.place(x=100,y=100)
         self.methodBtn.place(x=100,y=400)
+        self.histoBtn.place(x=100,y=570)
 
         self.methodAScale.place(x=250,y=350)
         self.methodBScale.place(x=250,y=400)
+        self.sizeChangeScale.place(x=250,y=450)
+        self.rotateScale.place(x=250,y=500)
 
         self.oriLbl.place(x=250,y=50,width=str(self.WIDTH_SIZE),height=str(self.HEIGHT_SIZE))
         self.modLbl.place(x=600,y=50,width=str(self.WIDTH_SIZE),height=str(self.HEIGHT_SIZE))
+        self.sizeChangeLbl.place(x=100,y=470)
+        self.rotateLbl.place(x=100,y=520)
 
     def selectFile(self):
         ifile = tkinter.filedialog.askopenfile(initialdir=os.path.abspath('.'),mode='rb',title='Choose File',filetypes=[("image files",".jpg .png")])
@@ -56,6 +93,7 @@ class DIPGUI(Frame):
         self.imgArray=np.asarray(self.oriImg)
         imgTmp = self.oriImg.resize((self.WIDTH_SIZE, self.HEIGHT_SIZE),Image.ANTIALIAS)
         self.modImgBool=True
+        self.modImg=imgTmp
 
         imgTmp = ImageTk.PhotoImage(imgTmp)
 
@@ -86,7 +124,7 @@ class DIPGUI(Frame):
             self.methodAScale.config(from_=0,to=1,resolution=0.005,digits=5)
             self.methodBScale.config(from_=-50,to=0,resolution=0.5,digits=2)
         elif nextMode==2:
-            self.methodAScale.config(from_=0,to=100000,resolution=100)
+            self.methodAScale.config(from_=0,to=100,resolution=1)
             self.methodBScale.config(from_=1,to=50,resolution=10)
             self.methodAScale.set(0)
             self.methodBScale.set(1)
@@ -98,12 +136,98 @@ class DIPGUI(Frame):
         mode=self.methodList.index(self.methodBtn['text'])
         a=self.methodAScale.get()
         b=self.methodBScale.get()
+
         if mode==0:
             imgTmpArray=a*self.imgArray+b
-            condition=imgTmpArray > 255
-            imgTmpArray[condition]=255
-            imgTmp=Image.fromarray(imgTmpArray)
-            imgTmp.resize((self.WIDTH_SIZE,self.HEIGHT_SIZE),Image.ANTIALIAS)
-            imgTmp=ImageTk.PhotoImage(imgTmp)
-            self.modLbl.configure(image=imgTmp)
-            self.modLbl.image=imgTmp
+
+
+        elif mode==1:
+            imgTmpArray=np.exp(a*self.imgArray+b)
+            
+
+        elif mode==2:
+            imgTmpArray=np.log(a*self.imgArray+b)
+
+        outlier=imgTmpArray > 255
+        imgTmpArray[outlier]=255
+
+        imgTmp=Image.fromarray(np.uint8(imgTmpArray))
+        imgTmp=imgTmp.resize((self.WIDTH_SIZE,self.WIDTH_SIZE),Image.ANTIALIAS)
+        self.modImg=imgTmp
+        imgTmp=ImageTk.PhotoImage(imgTmp)
+        self.modLbl.configure(image=imgTmp)
+        self.modLbl.image=imgTmp
+
+    def sizeChange(self,test):
+        time=2**(self.sizeChangeScale.get())
+        print(time)
+        print(self.imgArray.shape)
+        newShape=list(map(int,[self.imgArray.shape[0]*time,self.imgArray.shape[1]*time]))
+        sizeImg=np.zeros(newShape,dtype=np.uint8)
+        print(sizeImg.shape)
+
+        ratio=1/time
+        print(ratio)
+
+        for x in range(sizeImg.shape[0]):
+            for y in range(sizeImg.shape[1]):
+                oldX=ratio*x
+                oldY=ratio*y
+                sizeImg[x,y]=bilinear(self.imgArray,oldX,oldY)
+
+        self.modLbl.destroy()
+        imgTmp=Image.fromarray(np.uint8(sizeImg))
+        imgTmp=imgTmp.resize((sizeImg.shape[0],sizeImg.shape[1]),Image.ANTIALIAS)
+        self.modImg=imgTmp
+        imgTmp=ImageTk.PhotoImage(imgTmp)
+        self.modLbl=Label(bg="white",text="modified",font=("Courier",30),image=imgTmp)
+        self.modLbl.image=imgTmp
+        self.modLbl.place(x=600,y=50)
+
+    def rotate(self,test):
+        degree=self.rotateScale.get()
+        self.modLbl.destroy()
+        imgTmp=Image.fromarray(np.uint8(self.imgArray))
+        imgTmp=imgTmp.resize((self.WIDTH_SIZE,self.HEIGHT_SIZE),Image.ANTIALIAS)
+        imgTmp=imgTmp.rotate(degree)
+        self.modImg=imgTmp
+        imgTmp=ImageTk.PhotoImage(imgTmp)
+        self.modLbl=Label(bg="white",text="modified",font=("Courier",30),image=imgTmp)
+        self.modLbl.image=imgTmp
+        self.modLbl.place(x=600,y=50)
+
+    def histogram(self):
+        height=self.imgArray.shape[0]
+        width=self.imgArray.shape[1]
+
+        hisCount=np.zeros([256],np.int32)
+
+        for h in range(0,height):
+            for w in range(0,width):
+                hisCount[self.imgArray[h,w]]+=1
+
+        hisPdf=hisCount/hisCount.sum()
+        hisCdf=np.zeros(256,float)
+
+        hisCdf[0] = hisPdf[0]
+        for i in range(1, 256):
+            hisCdf[i] = hisCdf[i-1] + hisPdf[i]
+
+        sK = np.round(hisCdf*255, 0)
+        result = np.zeros((height,width))
+
+        for h in range(0,height):
+            for w in range(0,width):
+                oriLevel = self.imgArray[h, w]
+                newLevel = sK[oriLevel]
+                result[h,w] = newLevel
+
+        overfit=result > 255
+        result[overfit]=255
+
+        underfit=result < 0
+        result[underfit]=0
+        imgTmp=Image.fromarray(np.uint8(result))
+        imgTmp=ImageTk.PhotoImage(imgTmp)
+        self.modLbl.configure(image=imgTmp)
+        self.modLbl.image=imgTmp
